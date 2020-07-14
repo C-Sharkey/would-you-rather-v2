2ea4ddd7f6b1acad86ec771c17b4649e8fb7e81c
C Sharkey <colinsharkey88@hotmail.com>
2020-07-14 22:10:45 +0100

    Redux reducers created - index - users - questions - authedUser
    Wrapped provider on App

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
+
+document.getElementById('root')
 );
