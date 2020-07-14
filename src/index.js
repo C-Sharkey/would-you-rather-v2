65423e4206c49599574d2d4dc44f37cb58d3c207
C Sharkey <colinsharkey88@hotmail.com>
2020-07-14 22:57:25 +0100

    Typo fix and folder update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
 
-const store = createStore(reducer, middleware)
+const store = createStore(reducer, compose(middleware,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+  ))
 
 
 ReactDOM.render(
