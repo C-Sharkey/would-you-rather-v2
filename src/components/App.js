d41987abdb74a76c8f71ed33c73bab4acc4d3edd
C Sharkey <colinsharkey88@hotmail.com>
2020-07-14 22:56:55 +0100

    Folder restructure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.css';
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
+
+class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData)
+  }
+  render (){
+    return (
+      <div className="App">
+        Would you rather 2...
+      </div>
+    );
+  }
+
+}
+export default connect()(App);
+
