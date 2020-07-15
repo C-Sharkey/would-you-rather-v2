1c08dabbdb6a651fba51680e870c4236f5eacff1
C Sharkey <colinsharkey88@hotmail.com>
2020-07-15 08:39:06 +0100

    Updated Redux devtool extention

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
 
-const store = createStore(reducer, compose(middleware,
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-  ))
+const store = createStore(
+  reducer, 
+  compose(
+    middleware,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
+)
 
 
 ReactDOM.render(
 
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider>,
 
 
