f5e3f19a2b88856b45205779faa5af72a115c563
C Sharkey <colinsharkey88@hotmail.com>
2020-07-16 22:55:27 +0100

    Reducers for create questions

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,4 @@
-import { RECEIVE_QUESTIONS} from '../actions/questions'
+import { RECEIVE_QUESTIONS, ADD_QUESTION_ANSWER, ADD_QUESTION } from '../actions/questions'
 
 export default function questions (state = {}, action) {
     switch(action.type) {
@@ -7,6 +7,28 @@ export default function questions (state = {}, action) {
                  ...state,
                  ...action.questions
              }
+
+        case ADD_QUESTION_ANSWER:
+            const { authedUser, questionID, answer } = action.authedUser
+
+            return {
+                ...state, 
+                [questionID]:{
+                    ...state[questionID],
+                    [answer]: {
+                        ...state[questionID][answer],
+                        votes: state[questionID][answer].votes.concat([authedUser])
+                    }
+                }
+            }
+
+            case ADD_QUESTION:
+                const { question } = action
+                return {
+                    ...state,
+                    [question.id]: question
+                }
+
         default : 
             return state
     }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,4 @@
-import { RECEIVE_USERS } from '../actions/users'
+import { RECEIVE_USERS, ADD_ANSWER_USER, ADD_QUESTION_USER } from '../actions/users'
 
 export default function users (state = {}, action) {
     switch(action.type) {
@@ -7,6 +7,30 @@ export default function users (state = {}, action) {
                  ...state,
                  ...action.users
              }
+        case ADD_ANSWER_USER:
+            const { authedUser, questionID, answer} = action.authedUser
+
+            return {
+                ...state, 
+                [authedUser]: {
+                    ...state[authedUser],
+                    answers: {
+                        ...state[authedUser].answers,
+                        [questionID]: answer
+                    }
+                }
+            }
+
+        case ADD_QUESTION_USER:
+            const { question } = action
+            return {
+                ...state,
+                [question.author]: {
+                    ...state[question.author],
+                    question: state[question.author].questions.concat([question.id])
+                }
+            }
+
         default : 
             return state
     }
