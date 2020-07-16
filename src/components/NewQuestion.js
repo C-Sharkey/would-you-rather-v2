e9b0de9090733d7c10ad7ccf6bd01eabd239d000
C Sharkey <colinsharkey88@hotmail.com>
2020-07-16 22:57:12 +0100

    Created basic form for new question - 2 inputs - handled with state - dispatched to create new question

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { handleAddQuestion } from '../actions/shared'
 
 class NewQuestion extends Component {
+    state = {
+        inputOne: '',
+        inputTwo: '',
+    }
+
+    handleInputOne = (e) => {
+        this.setState({
+            inputOne: e.target.value
+        })
+    }
+    handleInputTwo = (e) => {
+        this.setState({
+            inputTwo: e.target.value
+        })
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        const { inputOne, inputTwo } = this.state
+        const { dispatch, authedUser } = this.props
+        dispatch(handleAddQuestion(inputOne, inputTwo, authedUser ))
+        this.props.history.push('/')
+    }
+
     render(){
+        const { inputOne, inputTwo } = this.state
+
         return(
             <div>
-                New Question
+                <h1>Create a new question</h1>
+                <form>
+                    <p>Would you rather:</p>
+                    <label htmlFor=''>Option 1</label>
+                    <input 
+                        type='text'
+                        value={inputOne} 
+                        onChange={this.handleInputOne}
+                        required 
+                    />
+                    <p>Or...</p>
+                    <label htmlFor=''>Option 2</label>
+                    <input 
+                        type='text' 
+                        value={inputTwo} 
+                        onChange={this.handleInputTwo}
+                        required 
+                    />
+                    <button 
+                        type='submit'
+                        onClick={this.handleFormSubmit}
+                    >
+                        Save Question
+                    </button>
+                </form>
             </div>
         )
     } 
