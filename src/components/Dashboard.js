f6bf5512ee4bbdbfb1658e6c3bce49f191ae59f6
C Sharkey <colinsharkey88@hotmail.com>
2020-07-18 13:54:44 +0100

    Added Toggle for Questions - renders unAnsweredQu by default - shows count of questions in each tab

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,36 +1,80 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Question from './Question'
+// React tabs for tab functionality 
+// referenced this tutorial: https://www.youtube.com/watch?v=tBaBl7gpYhs
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
+
 
 class Dashboard extends Component {
+    state = {
+        tabSelectedItem: 'unAnsweredQuestions'
+    }
     render(){
-        console.log(this.props)
-        const { questionCount, questionIds } = this.props
+        const { questionCount, questionsAnswered, questionsUnanswered } = this.props
+        const unAnsQuCount = questionsUnanswered.length
+        const AnsQuCount = questionsAnswered.length
+
+        const ansQu = questionsAnswered.map((answer) => (
+            <li key={answer.id} >
+                <Question id={answer.id} />
+            </li>
+            )) 
+        const unAnsQu = questionsUnanswered.map((answer) => (
+            <li key={answer.id} >
+                <Question id={answer.id} />
+            </li>
+            ))             
+         
         return(
             <div>
                 <h1>Dashboard</h1>
-                <h2>Questions: {questionCount} </h2>
-                <div className='container'>
-                    <ul>
-                        {questionIds.map((id) => (
-                            <li key={id}>
-                                <Question id={id} />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <h2>Total Questions: {questionCount} </h2>
+
+                <Tabs selected={this.state.tabSelectedItem} onChange={tabSelectedItem => this.setState({ tabSelectedItem })}>
+                    <TabList>
+                        <Tab item="unAnsweredQuestions">Unanswered Questions ({unAnsQuCount})</Tab>
+                        <Tab item="AnsweredQuestions">Answered Questions ({AnsQuCount})</Tab>
+                    </TabList>
+                    <TabPanel item="unAnsweredQuestions">
+                        <div className='container'>
+                            <ul>
+                                {unAnsQu}
+                            </ul>
+                        </div>
+                    </TabPanel>
+                    <TabPanel item="AnsweredQuestions">
+                        <div className='container'>
+                            <ul>
+                                {ansQu}
+                            </ul>
+                        </div>
+                    </TabPanel>
+                </Tabs>
             </div>
         )
     }
 }
 
-function mapStateToProps ({ questions }, { authedUser }) {
+function mapStateToProps ({ users, questions, authedUser }) {
+
     const questionCount = Object.keys(questions).length
+    const myAnswers = users[authedUser].answers
+    const getQuAns = Object.values(questions).filter((question) => {
+        return Object.keys(myAnswers).includes(question.id)
+      }).sort((a, b) => b.timestamp - a.timestamp)
+    const getQuUnAns = Object.values(questions).filter((question) => {
+        return !(Object.keys(myAnswers).includes(question.id))
+      }).sort((a, b) => b.timestamp - a.timestamp) 
+
     return {
         questionIds: Object.keys(questions)
             .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
         authedUser,
-        questionCount    
+        questionCount,
+        questionsAnswered: getQuAns, 
+        questionsUnanswered: getQuUnAns,    
     }
 }
 
