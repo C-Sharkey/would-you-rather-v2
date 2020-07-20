60637f066e19cd66e5de9d59e0954a3f305feeba
C Sharkey <colinsharkey88@hotmail.com>
2020-07-20 12:28:44 +0100

    Bootsrap styling for NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleSetAuthedUser } from '../actions/authedUser'
 import { NavLink } from 'react-router-dom'
+import { Navbar, Nav, Form, Badge, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class NavBar extends Component {
@@ -13,35 +16,23 @@ handleSignOut = (e) => {
 
     render() {
         const { user } = this.props
-        console.log('Navbar loggedInUser : ', user)
        
         return (
             <div>
-                <ul className='nav'>
-                    <li>
-                        <NavLink to='/'  activeClassName='active'>
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/new'  activeClassName='active'>
-                        New Question
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/leaderboard'  activeClassName='active'>
-                        Leaderboard
-                        </NavLink>
-                    </li>
-                    <li>
-                        Signed In: {user}
-                    </li>
-                    <li>
-                        <NavLink to='/' onClick={this.handleSignOut}>
-                        Sign Out
-                        </NavLink>
-                    </li>
-                </ul>
+
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand>Would You Rather</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link><NavLink to='/'  activeClassName='active'>Home</NavLink></Nav.Link>
+                    <Nav.Link><NavLink to='/new'  activeClassName='active'> New Question </NavLink></Nav.Link>
+                    <Nav.Link><NavLink to='/leaderboard'  activeClassName='active'>Leaderboard</NavLink></Nav.Link>
+                </Nav>
+                <Form inline>
+                    <Button variant="light" className='mr-3'>Welcome, {user}</Button>
+                    <NavLink to='/' onClick={this.handleSignOut}><Button variant="outline-info">Sign Out</Button></NavLink>
+                </Form>
+            </Navbar>
+
             </div>
         )
     }
